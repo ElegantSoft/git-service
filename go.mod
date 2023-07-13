61d55842449d5426f394607d6f5f96dad71b4324
ElegantSoft <react.laravel.dev@gmail.com>
2023-07-13 18:58:36 +0300

    finish

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ElegantSoft/git-service
 
 go 1.19
+
+require (
+	github.com/go-playground/webhooks/v6 v6.2.0 // indirect
+	github.com/google/go-github/v39 v39.2.0 // indirect
+	gopkg.in/go-playground/webhooks.v5 v5.17.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,1 +1,55 @@
-package git_service
+package main
+
+import (
+	"fmt"
+	"github.com/go-playground/webhooks/v6/github"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+)
+
+func main() {
+	// Create a new GitHub webhook instance
+	hook, _ := github.New(github.Options.Secret(os.Getenv("WEB_HOOK_SECRET")))
+
+	// Start the server and listen for incoming GitHub webhook events
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+		payload, err := hook.Parse(r, github.PushEvent)
+		if err != nil {
+			if err == github.ErrEventNotFound {
+				// Ignore requests that are not for push events
+				return
+			}
+			log.Printf("Failed to parse webhook payload: %s", err)
+			return
+		}
+
+		// Handle the parsed webhook payload
+		switch payload.(type) {
+
+		case github.PushPayload:
+			pushEvent := payload.(github.PushPayload)
+			fmt.Printf("%+v", pushEvent)
+			// Do whatever you want from here...
+			if pushEvent.Ref == fmt.Sprintf("refs/heads/%v", os.Getenv("BRANCH_NAME")) && pushEvent.Repository.FullName == os.Getenv("REPO_NAME") {
+				// Run the specific .sh file
+				cmd := exec.Command("bash", os.Getenv("SHELL_PATH"))
+				cmd.Stdout = os.Stdout
+				cmd.Stderr = os.Stderr
+				err := cmd.Run()
+				if err != nil {
+					log.Printf("Failed to run script: %s", err)
+				}
+			}
+
+		case github.PullRequestPayload:
+			pullRequest := payload.(github.PullRequestPayload)
+			// Do whatever you want from here...
+			fmt.Printf("%+v", pullRequest)
+		}
+	})
+
+	log.Printf("Starting server on :%s...", os.Getenv("PORT"))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", os.Getenv("port")), nil))
+}
